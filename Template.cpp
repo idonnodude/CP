a00f45a6483b33d001fab60a0a6cbbd178524229
idonnodude <59884936+idonnodude@users.noreply.github.com>
2020-10-01 00:43:23 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,31 @@
+/********************Code by Yash***************************
+************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+
+#define ll long long
+#define endl "\n"
+#define pb push_back
+#define F(i, a, b) for(i = a; i < b; ++i)
+#define mod 1000000007
+#define all(c) c.begin(), c.end()
+#define tr(container, it) for(auto it = container.begin(); it != container.end(); it++)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        
+    }
+}
